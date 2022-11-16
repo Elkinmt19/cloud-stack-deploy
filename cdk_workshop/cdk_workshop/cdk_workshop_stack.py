9a48e365a5ff267ce35fb25443ea1792e2134f80
Elkin Javier Guerra Galeano <elkinmt19@gmail.com>
2022-11-16 09:59:04 -0500

    Added a simple test application using AWS CDK

---
+++ cdk_workshop/cdk_workshop/cdk_workshop_stack.py
@@ -0,0 +1,24 @@
+from constructs import Construct
+from aws_cdk import (
+    Stack,
+    aws_lambda as _lambda,
+    aws_apigateway as apigw,
+)
+
+
+class CdkWorkshopStack(Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        my_lambda = _lambda.Function(
+            self, 'HelloHandler',
+            runtime=_lambda.Runtime.PYTHON_3_7,
+            code=_lambda.Code.from_asset('lambda'),
+            handler='hello.handler',
+        )
+
+        apigw.LambdaRestApi(
+            self, 'Endpoint',
+            handler=my_lambda,
+        )
